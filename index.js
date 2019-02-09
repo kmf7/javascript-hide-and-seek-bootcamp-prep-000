00eb80c1809f1f0a91571312afbb877d7efc9453
Kimberly Fritter <littlekimmy44@hotmail.com>
2019-02-09 18:06:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function getFirstSelector(selector){
   return document.querySelector(selector);
 }
 function nestedTarget(){
-  return document.querySelector("#nested.target");
+  return document.querySelector("#nested .target");
   }
 function increaseRankBy(n){
   const lis= document.getElementById("app")
